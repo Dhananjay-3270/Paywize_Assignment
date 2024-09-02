8bf2a320c0d45e4990fd1b39c9f85497264618a8
Dhananjay-3270 <shinde3270@gmail.com>
2024-09-03 00:28:56 +0530

    Added Canvas and Toolset Comp

--- Draw/src/App.jsx
+++ Draw/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-
 import './App.css'
-
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import Canvas from './Components/Canvas'
+import Toolbar from './Components/Toolbar'
 function App() {
 
 
   return (
-    <>
-     
-    
-      
-    </>
+    <Provider store={store}>
+   <div className='main'>
+<Canvas/>
+<Toolbar/>
+
+   </div>
+    </Provider>
   )
 }
 
